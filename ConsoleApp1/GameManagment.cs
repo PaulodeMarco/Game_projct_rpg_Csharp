d955a0e2e70198384a2fd5d1bfa8f8075e045934
PaulodeMarco <146986151+PaulodeMarco@users.noreply.github.com>
2023-10-30 14:55:45 -0300

    Jogo_RPG_Csharp
    Trabalho de final de matéria...

---
+++ ConsoleApp1/GameManagment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Konsole;
+
+namespace CRPG
+{
+    class GameManagment
+    {
+        public static void AgradecimentosFinais()
+        {
+            Console.Clear();
+
+            var agradecimento = Window.OpenBox("Parabéns!!!", 40, 5);
+            agradecimento.WriteLine("Você conseguiu terminar o jogo.");
+            agradecimento.WriteLine("Obrigado por jogar.");
+
+            Console.ReadKey();
+
+            Environment.Exit(0);
+        }
+    }
+}
